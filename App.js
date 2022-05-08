d4ed07b7dd3993abdb9ff50036f62f3ea3c9f28a
Zytha <yangyong080228@gmail.com>
2022-05-08 10:41:47 +0800

    🔥 First Commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View,Linking } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <Text style={styles.title}>Hello World</Text>
+            <Button title="Homepage" color="#242424" onPress={ () => Linking.openURL('https://www.zytha.ml')} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    title: {
+        fontFamily: 'Roboto',
+        fontSize: 48,
+        fontWeight: 'bold',
+        color: '#000',
+    }
 });
